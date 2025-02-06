c14a0e48f29d8a53ca6781c8da42b951e5e39750
simran-nayak <simrann881@gmail.com>
2025-02-05 23:24:47 -0800

    database attributes added

--- database/script.sql
+++ database/script.sql
@@ -1,39 +1,142 @@
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_email VARCHAR(255) UNIQUE NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    user_phone VARCHAR(20),
+    user_address TEXT,
+    user_role ENUM('USER','ADMIN','SELLER') DEFAULT 'USER',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255),
 );
 
 CREATE TABLE product (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    product_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
-    category VARCHAR(100) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
+    product_category VARCHAR(100) NOT NULL,
+    category_id VARCHAR(255) NOT NULL,
+    seller_id INT NOT NULL,
+    product_title VARCHAR(255) NOT NULL,
+    product_price DECIMAL(10,2) NOT NULL,
+    product_stock_quantity INT NOT NULL CHECK (stock_quantity >=0),
+    product_image_url TEXT NOT NULL,
+    product_status ENUM('ACTIVE','INACTIVE') DEFAULT 'ACTIVE',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE product_category (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    category VARCHAR(100) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
+CREATE TABLE category (
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    category_title VARCHAR(255) NOT NULL,
+    category_description TEXT NULL,
+    category_price DECIMAL(10,2) NOT NULL,
+    category_image_url TEXT NOT NULL,
+    category_status ENUM ('ACTIVE','INACTIVE') DEFAULT 'ACTIVE'
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE order_table (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     total_amount DECIMAL(10,2) NOT NULL,
     status ENUM('pending', 'shipped', 'delivered', 'canceled') DEFAULT 'active',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE OrderList (
+    Order_ID INT PRIMARY KEY AUTO_INCREMENT,
+    order_item_id VARCHAR(255) PRIMARY KEY,
+    Customer_ID INT NOT NULL,
+    Order_Date DATE NOT NULL,
+    Product_ID INT NOT NULL,
+    Quantity INT NOT NULL,
+    order_item_unit_price DECIMAL(10,2) NOT NULL,
+    TotalAmount DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
+CREATE TABLE Reviews (
+    Review_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Product_ID INT NOT NULL,
+    user_ID INT NOT NULL,
+    Rating INT NOT NULL CHECK (Rating >= 1 AND Rating <= 5),
+    ReviewText TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+CREATE TABLE Payments (
+    Payment_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Order_ID INT NOT NULL,
+    Payment_Method VARCHAR(50) NOT NULL,
+    Amount_Paid DECIMAL(10, 2) NOT NULL,
+    Payment_Status VARCHAR(20) NOT NULL CHECK (PaymentStatus IN ('Pending', 'Completed', 'Failed', 'Refunded')),
+    transaction_id VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255),
+    FOREIGN KEY (OrderID) REFERENCES OrderList(OrderID)
+);
+
+CREATE TABLE Sellers_payment (
+    Payment_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Order_ID INT NOT NULL,
+    Payment_Method VARCHAR(50) NOT NULL,
+    Amount_Paid DECIMAL(10, 2) NOT NULL,
+    Payment_Date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Payment_Status VARCHAR(20) NOT NULL CHECK (PaymentStatus IN ('Pending', 'Completed', 'Failed', 'Refunded')),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255),
+    FOREIGN KEY (OrderID) REFERENCES OrderList(OrderID)
+);
+
+CREATE TABLE Cart (
+    Cart_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Customer_ID INT NOT NULL,
+    Product_ID INT NOT NULL,
+    Quantity INT NOT NULL CHECK (Quantity > 0),
+    Date_Added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
+CREATE TABLE Wishlist (
+    Wishlist_ID INT PRIMARY KEY AUTO_INCREMENT,
+    user_ID INT NOT NULL,
+    Product_ID INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
 );
