e7a702e683525e7bba2668fdbf422f70e17f86bf
AdityaKhandwal <adityakhandual5@gamil.com>
2025-02-22 00:37:42 +0530

    signup page added

--- src/main/java/edu/rims/Fash_in/controller/SignInController.java
+++ src/main/java/edu/rims/Fash_in/controller/SignInController.java
@@ -2,11 +2,20 @@ package edu.rims.Fash_in.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import edu.rims.Fash_in.entity.User;
 
 @Controller
 public class SignInController {
     @GetMapping("/customer/sign-in")
     String signIn() {
         return "customer/sign-in";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUp(@ModelAttribute User user){
+        return "redirect:/customer/login";
     }
 }
