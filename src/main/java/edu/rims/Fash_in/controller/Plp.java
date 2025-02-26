005df145d19cb17643cbff3f40b5de74a591e96c
AdityaKhandwal <adityakhandual5@gamil.com>
2025-02-26 23:53:03 +0530

    fragments added

--- src/main/java/edu/rims/Fash_in/controller/Plp.java
+++ src/main/java/edu/rims/Fash_in/controller/Plp.java
@@ -6,19 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import edu.rims.Fash_in.entity.Category;
 import edu.rims.Fash_in.entity.Product;
+import edu.rims.Fash_in.repository.CategoryRepository;
 import edu.rims.Fash_in.repository.ProductRepository;
 
 
 @Controller
 public class Plp {
     @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
     private ProductRepository productRepository;
+
     @GetMapping("/customer/plp")
-    String plp(Model model) {
-        List<Product> product =productRepository.findAll();
-        model.addAttribute("products", product);
+    String plp(@RequestParam("category") String categoryId,Model model) {
+        Category category = categoryRepository.findById(categoryId).orElseThrow();
+        model.addAttribute("category", category);
         return "customer/plp";
     }
 }
