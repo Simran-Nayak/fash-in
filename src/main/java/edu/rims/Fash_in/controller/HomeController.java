eb6bfeb8df37e654d12d7333b02eef4552d8bc07
simran-nayak <simrann881@gmail.com>
2025-02-28 23:17:37 -0800

    fragment added to home page

--- src/main/java/edu/rims/Fash_in/controller/HomeController.java
+++ src/main/java/edu/rims/Fash_in/controller/HomeController.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.rims.Fash_in.entity.Category;
 import edu.rims.Fash_in.repository.CategoryRepository;
 
 @Controller
+// @RequestMapping("/customer")
 public class HomeController {
     @Autowired
     private CategoryRepository categoryRepository;
