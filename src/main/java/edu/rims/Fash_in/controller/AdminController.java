03b44499cd4f3b49e24e6e273267e0381d30f32e
AdityaKhandwal <adityakhandual5@gamil.com>
2025-03-06 10:18:39 +0530

    some changes

--- src/main/java/edu/rims/Fash_in/controller/AdminController.java
+++ src/main/java/edu/rims/Fash_in/controller/AdminController.java
@@ -1,16 +1,29 @@
 package edu.rims.Fash_in.controller;
 
-import java.util.Locale.Category;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.rims.Fash_in.entity.Product;
+import edu.rims.Fash_in.repository.ProductRepository;
 
 @Controller
 public class AdminController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
     @GetMapping("/customer/admin")
-    String cart() {
+    String cart(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
         return "customer/admin";
     }
 }
+
+/*@GetMapping("/Search")
+ * String search(@RequestParam String search)
+ */
