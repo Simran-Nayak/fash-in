9bd272dcd75e62fe4ba158138847f18296cc1991
simran-nayak <simrann881@gmail.com>
2025-03-11 22:09:27 -0700

    image upload to dashboard

--- src/main/java/edu/rims/Fash_in/FashInApplication.java
+++ src/main/java/edu/rims/Fash_in/FashInApplication.java
@@ -1,13 +1,24 @@
 package edu.rims.Fash_in;
 
+import java.io.File;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
  
 @SpringBootApplication
-public class FashInApplication {
+public class FashInApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 	SpringApplication.run(FashInApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception{
+		File file = new File("upload_images") ;
+		if (!file.exists()){
+			file.mkdir();
+		}
 	}
 }
     
